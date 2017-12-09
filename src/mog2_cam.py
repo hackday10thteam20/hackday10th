bbf32b7ce0ee13023fa242887ea712eee9cadea8
爲房　新太朗 <stamefus@MBP-13JAR-081.local>
2017-12-10 08:21:23 +0900

    評価を変えようとしてうまくいかなかったけどコミットだけする

--- src/mog2_cam.py
+++ src/mog2_cam.py
@@ -11,6 +11,17 @@ def calcVal(img, x):
             val+=1
     return val
 
+# 全画素中の白をカウントする
+def calcAllVal(img):
+    return cv2.countNonZero(img)
+
+# 任意のx座標での列を返却する
+def getColumn(img, x):
+    return img[:, x]
+
+def calcVal2(arr1, arr2):
+    return np.sum(abs((arr1-arr2)/255))
+
 # ここからメインの処理
 
 # 音周りの初期化
@@ -27,7 +38,9 @@ fgbg = cv2.createBackgroundSubtractorMOG2()
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
 
 # 物体が通過したかの値を管理
-vals = np.zeros(10)
+vals = np.zeros((2, 960))
+val = np.zeros(960)
+val_pre = np.zeros(960)
 loop = 0
 
 print('main process start')
@@ -51,18 +64,26 @@ while(1):
     # 表示
     cv2.imshow('frame',fgmask)
 
-    # 中央の通過数をカウント
+    # 通過数をカウント
     height, width = fgmask.shape[:2]
-    vals[loop] = calcVal(fgmask, int(width/2))
-    result = np.sum(vals)
+    pixels = width * height
+    val = getColumn(fgmask, int(width/2)) # 中央
+    #vals[0][loop] = getColumn(fgmask, int(width/2)) # 中央
+    #vals[1][loop] = getColumn(fgmask, int(width/4)) # 左半分
+    #vals[2][loop] = getColumn(fgmask, int(width/4*3)) # 右半分
+    #result = calcVal2(vals[0][0], vals[0][1]) + calcVal2(vals[1][0], vals[1][1]) +calcVal2(vals[2][0], vals[2][1])
+    result = calcVal2(val, val_pre)
     # 評価値として正規化
-    print(str(int(result/(height*len(vals))*100)) + " " + str(vals))
+    #print(str(result) + " " + str(val) + " " + str(val_pre))
+    print(str(result))
     # 音を鳴らす
-    jiroSound.play_sound(result, 9600)
+    jiroSound.play_sound(result, vals.size*height)
     # グラフを描画
     graph.drawing(result)
 
-    loop = (loop+1)%10
+    #print("loop: " + str(loop))
+    #loop = (loop+1)%vals.size
+    val_pre = val
             
 cap.release()
 cv2.destroyAllWindows()
