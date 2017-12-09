ebfc29913998d87f7e70ca0630a8c71e37e845f7
synboo <shu.shimbo@gmail.com>
2017-12-09 18:07:06 +0900

    リファクタリング: クラス化

--- jiro_sound.py
+++ jiro_sound.py
@@ -1,41 +1,52 @@
 import pygame.mixer
 import time
 
-# 音をならす
-def play_sound(level, sounds):
-  for i in range(level):
-    track = sounds[i]
-    pygame.mixer.Channel(i).play(track)
-  # 再生の終了
-  pygame.mixer.music.stop()
+# JiroSound
+class JiroSound:
 
-sound_files = [
-  "sound/se_maoudamashii_instruments_piano1_1do.wav",
-  "sound/se_maoudamashii_instruments_piano1_2re.wav",
-  "sound/se_maoudamashii_instruments_piano1_3mi.wav",
-  "sound/se_maoudamashii_instruments_piano1_4fa.wav",
-  "sound/se_maoudamashii_instruments_piano1_5so.wav",
-  "sound/se_maoudamashii_instruments_piano1_6ra.wav",
-  "sound/se_maoudamashii_instruments_piano1_7si.wav",
-  "sound/se_maoudamashii_instruments_piano1_8do.wav"
-]
+  # Sound file paths
+  sound_files = [
+    "sound/se_maoudamashii_instruments_piano1_1do.wav",
+    "sound/se_maoudamashii_instruments_piano1_2re.wav",
+    "sound/se_maoudamashii_instruments_piano1_3mi.wav",
+    "sound/se_maoudamashii_instruments_piano1_4fa.wav",
+    "sound/se_maoudamashii_instruments_piano1_5so.wav",
+    "sound/se_maoudamashii_instruments_piano1_6ra.wav",
+    "sound/se_maoudamashii_instruments_piano1_7si.wav",
+    "sound/se_maoudamashii_instruments_piano1_8do.wav"
+  ]
 
-# mixerモジュールの初期化
-pygame.init()
-pygame.mixer.set_num_channels(8)
+  sounds = []
 
-# 音楽ファイルの読み込み
-sounds = []
-i = 0
-for n in sound_files:
-  sounds.append(pygame.mixer.Sound(sound_files[i]))
-  i = i+1
+  # Init
+  def __init__(self):
+    # Init mixer module
+    pygame.init()
+    pygame.mixer.set_num_channels(8)
 
-#level = int(input('Input Level: '))
-#level = 1
-for level in range(8):
-  print(level)
-  play_sound(level, sounds)
-  time.sleep(1)
+    # Read sound files
+    i = 0
+    for n in self.sound_files:
+      self.sounds.append(pygame.mixer.Sound(self.sound_files[i]))
+      i = i+1
 
-time.sleep(60)
+  # Play sound
+  def play_sound(self, level):
+    for i in range(level):
+      track = self.sounds[i]
+      print(track)
+      pygame.mixer.Channel(i).play(track)
+    # Stop sound
+    pygame.mixer.music.stop()
+
+  # Play demo sounds
+  def play_demo_sounds(self):
+    for level in range(8):
+      print(level)
+      self.play_sound(level+1)
+      time.sleep(1)
+    time.sleep(60)
+
+
+jiroSound = JiroSound()
+jiroSound.play_demo_sounds()
